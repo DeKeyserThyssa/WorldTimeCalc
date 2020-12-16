6bf24aa843768ef97120634fedbda38cf0b0c21c
DeKeyserThyssa <thyssa.de.keyser@student.howest.be>
2020-12-16 19:20:07 +0100

    SVG probleem opgelost + landingspagina

--- script/reg.js
+++ script/reg.js
@@ -1,7 +1,19 @@
+function handleFloatingLabel() {
+    let input = document.querySelector('.js-floating-input'),
+        label = document.querySelector('.js-floating-label');
+
+    input.addEventListener('blur', function () {
+        console.log(event);
+        if (input.value) {
+            label.classList.add('is-floating');
+        } else {
+            label.classList.remove('is-floating');
+        }
+    });
+}
 /* -------------------------------------------------------------------------- */
 // Alle interactieve onderdelen voor onze site. We maken van alle inputs een object met zijn eigen eigenschappen.
 let email = {},
-	password = {},
 	signInButton;
 /* -------------------------------------------------------------------------- */
 
@@ -10,11 +22,6 @@ let email = {},
 const isValidEmailAddress = function(emailAddress) {
 	// Basis manier om e-mailadres te checken.
 	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailAddress);
-};
-
-const isValidPassword = function(password) {
-	// Het wachtwoord moet minstens 6 karakters bevatten, verder niks.
-	return password.length > 1;
 };
 
 const isEmpty = function(fieldValue) {
@@ -38,21 +45,6 @@ const doubleCheckEmailAddress = function() {
 	}
 };
 
-const doubleCheckPassword = function() {
-	if (isValidPassword(password.input.value)) {
-		// Stop met dit veld in de gaten te houden; het is in orde.
-		password.input.removeEventListener('input', doubleCheckPassword);
-		removeErrors(password);
-	} else {
-		// Stuk herhalende code.
-		if (isEmpty(password.input.value)) {
-			password.errorMessage.innerText = 'This field is required';
-		} else {
-			password.errorMessage.innerText = 'Invalid password';
-		}
-	}
-};
-
 const addErrors = function(formField) {
 	formField.field.classList.add('has-error');
 	formField.errorMessage.classList.add('is-visible');
@@ -71,31 +63,7 @@ const getDOMElements = function() {
 	email.input = document.querySelector('.js-email-input');
 	email.field = document.querySelector('.js-email-field');
 
-	password.label = document.querySelector('.js-password-label');
-	password.errorMessage = password.label.querySelector(
-		'.js-password-error-message'
-	);
-	password.input = document.querySelector('.js-password-input');
-	password.field = document.querySelector('.js-password-field');
-
-	// Optional
-	// remember.label = document.querySelector('.js-remember-label');
-	// remember.errorMessage = null; // Currently not available
-	// remember.field = document.querySelector('.js-remember-input');
-
 	signInButton = document.querySelector('.js-sign-in-button');
-
-	// Check jezelf; hover over de gelogde DOM elementen in de console.
-	// console.log(
-	// 	'email',
-	// 	email,
-	// 	'password',
-	// 	password,
-	// 	'remember',
-	// 	remember,
-	// 	'button',
-	// 	signInButton
-	// );
 };
 
 const enableListeners = function() {
@@ -107,23 +75,9 @@ const enableListeners = function() {
 				email.errorMessage.innerText = 'Invalid emailaddress';
 			}
 
-			addErrors(email);
-
-			// Gebruik een named function (doubleCheckPassword), om die er weer af te kunnen halen. Dit vermijdt ook het dubbel toevoegen ervan.
+            addErrors(email);
+            
 			email.input.addEventListener('input', doubleCheckEmailAddress);
-		}
-	});
-
-	password.input.addEventListener('blur', function() {
-		if (!isValidPassword(password.input.value)) {
-			if (isEmpty(email.input.value)) {
-				email.errorMessage.innerText = 'This field is required';
-			}
-
-			addErrors(password);
-
-			// Gebruik een named function (doubleCheckPassword), om die er weer af te kunnen halen. Dit vermijdt ook het dubbel toevoegen ervan.
-			password.input.addEventListener('input', doubleCheckPassword);
 		}
 	});
 
@@ -132,14 +86,11 @@ const enableListeners = function() {
 		e.preventDefault();
 
 		if (
-			isValidEmailAddress(email.input.value) &&
-			isValidPassword(password.input.value)
+			isValidEmailAddress(email.input.value)
 		) {
 			console.log('Form is good to go!');
 		} else {
 			// Stuk herhalende code...
-			addErrors(password);
-			password.input.addEventListener('input', doubleCheckPassword);
 
 			addErrors(email);
 			email.input.addEventListener('input', doubleCheckEmailAddress);
@@ -152,7 +103,9 @@ const enableListeners = function() {
 // We kunnen pas iets doen met onze html-content (DOM) als die geladen is.
 document.addEventListener('DOMContentLoaded', function() {
 	// Ook even testen of ik DoMConteeentLoeaaded goed geschreven heb...
-	console.log('DOM loaded 🥳!');
+    console.log('DOM loaded 🥳!');
+    
+    handleFloatingLabel();
 
 	// We splitsen alles netjes op in verschillende functies.
 	// 1. Alle linken leggen naar onze HTML.
